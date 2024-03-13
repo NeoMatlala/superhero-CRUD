115a0e6f3902895c23908cb5adc1a61c10e3bfd3
NeoMatlala <neomatlala70@gmail.com>
2024-03-13 15:38:18 +0200

    FE: Read single hero

--- superhero.UI/src/app/components/hero-edit/hero-edit.component.ts
+++ superhero.UI/src/app/components/hero-edit/hero-edit.component.ts
@@ -1,20 +1,44 @@
 import { Component } from '@angular/core';
 import { RouterLink, ActivatedRoute } from '@angular/router';
+import { SuperHeroService } from '../../services/super-hero.service';
+import { SuperHero } from '../../models/super-hero';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-hero-edit',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    FormsModule 
   ],
   templateUrl: './hero-edit.component.html',
   styleUrl: './hero-edit.component.css'
 })
 export class HeroEditComponent {
   heroId: number = 0
-  constructor(private router: ActivatedRoute) {
+
+  hero: SuperHero = {
+    heroId: 0,
+    name: '',
+    firstName: '',
+    lastName: '',
+    place: ''
+  }
+
+  constructor(private router: ActivatedRoute, private _superHeroService: SuperHeroService) {
     this.router.params.subscribe(params => {
       this.heroId = params['id']
     })
+  }
+
+  ngOnInit() : void {
+    this._superHeroService.getSingleSuperhero(this.heroId).subscribe(
+      (data: any) => {
+        this.hero = data.hero
+      },
+      error => {
+        console.error('Error occured while fetching heroe:', error)
+      }
+    )
   }
 }
